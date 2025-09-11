56f35007c6e712ce4e815be28672124c23c66944
aimeejcs <cristicaranfil8@gmail.com>
2025-09-11 11:26:43 +0200

    Assignment-1

--- Server/InMemoryRepositories/UserInMemoryRepository.cs
+++ Server/InMemoryRepositories/UserInMemoryRepository.cs
@@ -1,8 +1,69 @@
-using System;
+using Entities;
+using RepositoryContracts;
 
 namespace InMemoryRepositories;
 
-public class UserInMemoryRepository
+public class UserInMemoryRepository : IUserRepository
 {
 
+    private List<User> users;
+
+    public UserInMemoryRepository()
+    {
+        users = new List<User>
+        {
+            new User { Id = 1, UserName = "YxngHamzyz", Password = "Hanan123"},
+            new User { Id = 2, UserName = "Hamuushh", Password = "Sheikhdon10"}
+        };
+    }
+
+    public Task<User> AddAsync(User user)
+    {
+        user.Id = users.Any()
+            ? users.Max(u => u.Id) + 1
+            : 1;
+        users.Add(user);
+        return Task.FromResult(user);
+    }
+
+    public Task UpdateAsync(User user)
+    {
+        User? existingUser = users.SingleOrDefault(u => u.Id == user.Id);
+        if (existingUser is null)
+        {
+            throw new InvalidOperationException($"User with ID '{user.Id}' not found");
+        }
+        users.Remove(existingUser);
+        users.Add(user);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        User? userToRemove = users.SingleOrDefault(u => u.Id == id);
+        if (userToRemove is null)
+        {
+            throw new InvalidOperationException($"User with ID '{id}' not found");
+        }
+        users.Remove(userToRemove);
+        return Task.CompletedTask;
+    }
+
+    public Task<User> GetSingleAsync(int id)
+    {
+        User? userSingleToGet = users.SingleOrDefault(u => u.Id == id);
+        if (userSingleToGet is null)
+        {
+            throw new InvalidOperationException($"User with ID '{id}' not found");
+        }
+        return Task.FromResult(userSingleToGet);
+        
+    }
+
+    public IQueryable<User> GetManyAsync()
+    {
+        return users.AsQueryable();
+    }
+
 }
+
